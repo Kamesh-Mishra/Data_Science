2d96eb8150057743d16c5ad168da25f1803a0215
Kamesh-Mishra <58304583+Kamesh-Mishra@users.noreply.github.com>
2020-01-17 12:25:49 +0530

    Added new file

---
+++ Data Analytics/Numpy+Pandas code challenges/ecommerce.py
@@ -0,0 +1,36 @@
+"""
+Code Challenge
+  Name: 
+    E-commerce Data Exploration
+  Filename: 
+    ecommerce.py
+  Problem Statement:
+      To create an array of random e-commerce data of total amount spent per transaction. 
+      Segment this incomes data into 50 buckets (number of bars) and plot it as a histogram.
+      Find the mean and median of this data using NumPy package.
+      Add outliers 
+          
+  Hint:
+      Execute the code snippet below.
+      import numpy as np
+      import matplotlib.pyplot as plt
+      incomes = np.random.normal(100.0, 20.0, 10000)
+      print (incomes)
+ 
+    outlier is an observation that lies an abnormal distance from other values 
+    
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+r = np.random.normal(100.0, 20.0, 10000)
+
+
+
+print("MEAN : ",np.mean(r))
+print("MEDIAN : ",np.median(r))
+
+
+plt.hist(r, 20)
+plt.show()
