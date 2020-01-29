6284bf56938d59f86d7c7e561c9b41293c3e0139
Kamesh-Mishra <58304583+Kamesh-Mishra@users.noreply.github.com>
2020-01-29 13:59:52 +0530

    Added new file

--- JSON/3/Fb_Login_Selenium.py
+++ JSON/3/Fb_Login_Selenium.py
@@ -37,3 +37,70 @@ Explanation of the code
     Code line 12: close
     
 """
+
+
+
+
+
+#   https://www.guru99.com/selenium-python.html
+
+
+##########################################################################################
+
+
+
+
+"""
+
+
+EXAMPLE 2: Login into Facebook & Check Title
+
+In this example,
+
+    We will open a login page.
+    Fill the required field "username" and "password".
+    Check Page Title
+    
+    
+"""
+
+
+
+ from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+# Step 1) Open Firefox 
+browser = webdriver.Firefox()
+# Step 2) Navigate to Facebook
+browser.get("http://www.facebook.com")
+# Step 3) Search & Enter the Email or Phone field & Enter Password
+username = browser.find_element_by_id("email")
+password = browser.find_element_by_id("pass")
+submit   = browser.find_element_by_id("loginbutton")
+username.send_keys("YOUR EMAILID")
+password.send_keys("YOUR PASSWORD")
+# Step 4) Click Login
+submit.click()
+wait = WebDriverWait( browser, 5 )
+page_title = browser.title
+assert page_title == "Facebook"
+
+
+
+
+
+"""
+
+Explanation of the code:
+
+    Code line 1-2: Import selenium packages
+    Code line 4: Initialize Firefox by creating an object
+    Code line 6: Get login page (Facebook)
+    Code line 8-10: Fetch username, password input boxes and submit button.
+    Code line 11-12: Enter data into username and password input boxes
+    Code line 14: Click on the "Submit" button
+    Code line 15: Create wait object with a timeout of 5 sec.
+    Code line 16: Capturing the title from "browser" Object.
+    Code Line 17: Testing the captured title string with "Facebook"
+
+"""
+
