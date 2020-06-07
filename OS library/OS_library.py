c07c233b5f12c8c8e9fe4568985c1ea9704a0147
Kamesh-Mishra <58304583+Kamesh-Mishra@users.noreply.github.com>
2020-06-07 19:22:06 +0530

    Added new file

---
+++ OS library/OS_library.py
@@ -0,0 +1,25 @@
+
+
+'''Important Operating System related file handling'''
+     
+import os
+
+os.getcwd()
+
+os.chdir("/Users/ajoyfern/Google Drive/Skills_Dev/Courses/2019/Trainings_2019")
+
+os.getcwd()
+
+os.chdir("/Users/sylvester/Google Drive/Skills_Dev/Courses/2019/Trainings_2019/Python")
+
+os.path.exists("zoo.csv")
+
+os.path.exists("data/zoo.csv")
+
+os.remove("new.txt")
+
+os.makedirs("myfolder")
+
+os.rmdir("myfolder")
+
+os.listdir(".")
