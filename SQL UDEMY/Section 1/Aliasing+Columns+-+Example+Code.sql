b8a7480b57adc860db89ca1a46cd66c3034322ff
Kamesh-Mishra <58304583+Kamesh-Mishra@users.noreply.github.com>
2021-01-11 10:26:51 +0530

    Added new file

--- SQL UDEMY/Section 1/Aliasing+Columns+-+Example+Code.sql
+++
@@ -1,8 +0,0 @@
---Aliasing a column name in SQL
-SELECT 
-       [Organization Level] = [OrganizationLevel]
-      ,[Job Title] = [JobTitle]
-      ,[Hire Date] = [HireDate]
-      ,[Vacation Hours] = [VacationHours]
-	  
-FROM [AdventureWorks2019].[HumanResources].[Employee]
